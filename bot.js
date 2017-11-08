db57ff0f542e2248a4b3189fa7dfb8b2eff3e7b3
Deniernal354 <31352787+Deniernal354@users.noreply.github.com>
2017-11-08 08:57:24 -0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,4 +5,4 @@ client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`); });
 client.on('message', msg => { 
 if (msg.content === 'ping') {
 msg.reply('Pong!'); } }); 
-client.login('Mzc3ODcyNzg0NzU3NjIwNzM2.DOTTmA._-N0VM21Tluci4fbl4pzMAU7Vdk');
+client.login(process.env.BOT_TOKEN);
