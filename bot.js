ddb804cd690fda1f21012b26b0c105fb98540df0
Deniernal354 <31352787+Deniernal354@users.noreply.github.com>
2017-11-10 13:03:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@
 const Discord = require("discord.js"); 
 const client = new Discord.Client(); 
 client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`); }); 
-client.on('message', msg => { 
-if (msg.content === 'ping') {
-msg.reply('Pong!'); } }); 
+client.on('message', message => { 
+if (message.content === 'ping') {
+message.reply('Pong!'); } }); 
 client.login(process.env.BOT_TOKEN);
